8434d95479516540cce6f9e95da5d9e15d63c95c
Devam Shroff <dev.shroff12@gmail.com>
2020-05-31 07:08:19 +0000

    added locust files for tomcat

---
+++ project5/read_tomcat.py
@@ -0,0 +1,24 @@
+import sys, random
+from locust import HttpLocust, TaskSet
+
+def readPost(locust):
+    postid = random.randint(1,500)
+    url_prefix = '/editor/post?action=open';
+    locust.client.get(url_prefix + '&username=cs144&postid=' + str(postid), name=url_prefix)
+
+class MyTaskSet(TaskSet):
+    """ the class MyTaskSet inherits from the class TaskSet, defining the behavior of the user """
+    # tasks = {getList: 2, previewPage: 1}
+    tasks = [readPost]
+    def on_start(locust):
+        """ on_start is called when a Locust start before any task is scheduled """
+        response = locust.client.post("/login", data={"username":"cs144", "password": "password"})
+        if response.status_code != 200:
+            print("FAIL to start with posting data to server. Make sure that your server is running.")
+            sys.exit();
+
+class MyLocust(HttpLocust):
+    """ the class MyLocust inherits from the class HttpLocust, representing an HTTP user """
+    task_set = MyTaskSet
+    min_wait = 1000
+    max_wait = 2000
