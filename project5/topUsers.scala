96e2c7a2a9040e1cccdd6fc7c8c89b65b4db03e6
Devam Shroff <dev.shroff12@gmail.com>
2020-06-01 06:55:55 +0000

    got topUsers.scala, but there are missing counts

--- project5/topUsers.scala
+++ project5/topUsers.scala
@@ -10,26 +10,34 @@ object FollowerCount {
     val conf = new SparkConf().setAppName("Follower Count")
     val sc = new SparkContext(conf)
 
-    // attempting to create a map between user and followers
-    val myMap = scala.collection.mutable.Map[String, Array[String]]() 
-    Source.fromFile("twitter.edges").getLines.foreach {
-        line => myMap += (line.split(":")(0) -> line.split(": ")(1).split(","))
-    }
-
-    // alternative approach: just create a list of users and then count number of times it appeas in twitter.edges - 1
-
-    // now getting counts for each user
-    // for ((user,followers) <- myMap){
-        
+    // original map approach -- failed
+    // // attempting to create a map between user and followers
+    // val myMap = scala.collection.mutable.Map[String, Array[String]]() 
+    // Source.fromFile("twitter.edges").getLines.foreach {
+    //     line => myMap += (line.split(":")(0) -> line.split(": ")(1).split(","))
     // }
 
+    // Spark - MapReduce Approach
     val lines = sc.textFile("twitter.edges")
-    val words = lines.flatMap(
-      lines.split("\n").lines.split(":")(0) => 
-        lines.split("\n").line.split(": ")(1).split(","))
-    val word1s = words.map(word => (word, 1))
-    val wordCounts = word1s.reduceByKey((a,b) => a+b)
-    wordCounts.saveAsTextFile("output-users")
+    val splitRdd = lines.map(line => line.split(":"))
+    val yourRdd = splitRdd.flatMap( arr => {
+      val user = arr(0)
+      val followsList = arr(1)
+      val followedUser = followsList.split(",")
+      followedUser.map(followedUser => (followedUser, user))
+    })
+    
+    // yourRdd.foreach({case (followedUser, user) => println( s"{ $followedUser, $user") })
+
+    val countRdd = yourRdd.groupBy({
+      case (followedUser, user) => followedUser
+    }).map({
+       case (user, iter) => (user, iter.size) 
+    }).filter({
+      case (user, count) => count > 1000
+    })
+    
+    countRdd.saveAsTextFile("output-users")
 
     sc.stop()
   }
